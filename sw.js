013df68e109aa18a93a0bdd364a890026d64335f
kakashikempachi <kakashikempachi@gmail.com>
2026-03-23 16:52:39 -0300

    Update service worker to use relative paths

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 const CACHE_NAME = 'jblar-v1';
+// Note que removemos a barra inicial para o caminho ser relativo à pasta do repositório
 const ASSETS = [
-  '/',
-  '/index.html',
+  './',
+  './index.html',
   'https://raw.githubusercontent.com/kakashikempachi/Calculadora-JBLAR/refs/heads/main/file_000000002f64720e9e886d9bc8e6a94f.png'
 ];
 
