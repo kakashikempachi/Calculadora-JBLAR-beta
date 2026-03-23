4bfb3f5090fbd2a99c8d81e1288534eda8053d36
kakashikempachi <kakashikempachi@gmail.com>
2026-03-23 17:03:46 -0300

    Update service worker to version 2 with new assets

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
-const CACHE_NAME = 'jblar-v1';
-// Note que removemos a barra inicial para o caminho ser relativo à pasta do repositório
+const CACHE_NAME = 'jblar-v2';
 const ASSETS = [
-  './',
-  './index.html',
+  '/Calculadora-JBLAR-beta/',
+  '/Calculadora-JBLAR-beta/index.html',
+  '/Calculadora-JBLAR-beta/manifest.json',
   'https://raw.githubusercontent.com/kakashikempachi/Calculadora-JBLAR/refs/heads/main/file_000000002f64720e9e886d9bc8e6a94f.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
